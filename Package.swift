0b8b748d13fdc8fc60cbc52e797ca21922b35ba7
chanjima <chanjimasuke@gmail.com>
2025-03-06 00:31:50 +0900

    コードフォーマット

--- Package.swift
+++ Package.swift
@@ -6,15 +6,9 @@ import PackageDescription
 let package = Package(
     name: "MFMKit",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MFMKit",
-            targets: ["MFMKit"]),
+        .library(name: "MFMKit", targets: ["MFMKit"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "MFMKit",
-                          path: "MFMKit.xcframework"),
+        .binaryTarget(name: "MFMKit", path: "MFMKit.xcframework")
     ]
 )
